a0b5bc704d7cf171f564c707b4ec2e69129b2de3
Cameron Orosco <cameron.orosco@outlook.com>
2018-04-27 00:21:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var spy = "'HELLO'";
+function logShout(spy) {
+  return spy.toUpperCase()
+}
+console.log(spy)
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
